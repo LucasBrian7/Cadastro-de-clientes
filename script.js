ee9ea2c47680626c473433deaa7c0e76ad94476e
Lucas Brian <lucasbrian130502@gmail.com>
2026-01-13 17:30:33 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,79 @@
+const nome = document.getElementById("nome")
+const email = document.getElementById("email")
+const botao = document.getElementById("botao")
+const cadastroDeCliente = document.getElementById("cadastroDeCliente")
+
+const API_URL = "https://crudcrud.com/api/e273e62e91ee4967958d5d1bcd878fe6/clientes"
+
+// LISTAR CLIENTES (GET)
+function carregarClientes() {
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(clientes => {
+            cadastroDeCliente.innerHTML = ""
+
+            clientes.forEach(cliente => {
+                criarClienteNaTela(cliente)
+            })
+        })
+}
+
+carregarClientes()
+
+// CRIAR CLIENTE NA TELA
+function criarClienteNaTela(cliente) {
+    const li = document.createElement("li")
+    li.textContent = `${cliente.nome} - ${cliente.email}`
+
+    const botaoExcluir = document.createElement("button")
+    botaoExcluir.textContent = "Excluir"
+
+    botaoExcluir.addEventListener("click", function () {
+        excluirCliente(cliente._id, li)
+    })
+
+    li.appendChild(botaoExcluir)
+    cadastroDeCliente.appendChild(li)
+}
+
+// CADASTRAR CLIENTE (POST)
+botao.addEventListener("click", function () {
+    const nomeDigitado = nome.value.trim()
+    const emailDigitado = email.value.trim()
+
+    if (nomeDigitado === "" || emailDigitado === "") {
+        alert("Preencha nome e email")
+        return
+    }
+
+    const cliente = {
+        nome: nomeDigitado,
+        email: emailDigitado
+    }
+
+    fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(cliente)
+    })
+        .then(response => response.json())
+        .then(data => {
+            criarClienteNaTela(data)
+            nome.value = ""
+            email.value = ""
+        })
+})
+
+// EXCLUIR CLIENTE (DELETE)
+function excluirCliente(id, li) {
+    fetch(`${API_URL}/${id}`, {
+        method: "DELETE"
+    })
+        .then(() => {
+            li.remove()
+        })
+}
+
+
